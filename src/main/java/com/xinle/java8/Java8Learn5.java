f29c73d5b6d0bdb9effa8f3f678b5109b4e1ffe5
xinle <510403586@qq.com>
2017-07-28 09:57:17 +0800

    add code

--- src/main/java/com/xinle/java8/Java8Learn5.java
+++ src/main/java/com/xinle/java8/Java8Learn5.java
@@ -1,8 +1,6 @@
 package com.xinle.java8;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Java8Learn5 {
@@ -14,10 +12,13 @@ public class Java8Learn5 {
         Student student3 = new Student("wangwu", 80, 40);
         Student student4 = new Student("zhangsan", 80, 40);
 
-        Student student5 = new Student("zhangsan",100,20);
+        Student student5 = new Student("zhangsan",90,20);
+        Student student6 = new Student("lisi",100,20);
+        Student student7 = new Student("wangwu",90,20);
+        Student student8 = new Student("zhangsan",100,20);
 
 
-        List<Student> studentList = Arrays.asList(student1, student2, student3, student4,student5);
+        List<Student> studentList = Arrays.asList(student1, student2, student3, student4,student5, student6, student7, student8);
 
         Map<String, List<Student>> map = studentList.stream().collect(Collectors.groupingBy(Student::getName));
 
@@ -29,7 +30,36 @@ public class Java8Learn5 {
 
         Map<Boolean,List<Student>> map4 = studentList.stream().collect(Collectors.partitioningBy(item -> item.getAge() > 20));
 
+        studentList.stream().min(Comparator.comparingInt(Student::getScore)).ifPresent(System.out::println);
+
+        studentList.stream().max(Comparator.comparingInt(Student::getScore)).ifPresent(System.out::println);
+
+        studentList.stream().collect(Collectors.averagingInt(Student::getScore));
+
+        studentList.stream().mapToInt(Student::getScore).sum();
+
+        IntSummaryStatistics intSummaryStatistics = studentList.stream().collect(Collectors.summarizingInt(Student::getScore));
+
         System.out.println(map4);
+
+
+
+        String name = studentList.stream().map(Student::getName).collect(Collectors.joining());
+
+        System.out.println(name);
+
+        String name1 = studentList.stream().map(Student::getName).collect(Collectors.joining(" " , "hi", "hello"));
+
+        System.out.println(name1);
+
+
+
+
+        studentList.stream().collect(Collectors.groupingBy(Student::getScore, Collectors.groupingBy(Student::getName)));
+
+
+
+
 
 
 
--- src/main/java/com/xinle/java8/Student.java
+++ src/main/java/com/xinle/java8/Student.java
@@ -28,6 +28,15 @@ public class Student {
         return score;
     }
 
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", score=" + score +
+                ", age=" + age +
+                '}';
+    }
+
     public void setScore(int score) {
         this.score = score;
     }
